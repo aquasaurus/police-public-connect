a8abef6bff0d26bb35476095c32592cbc5e27c9d
raghuram <airaghuram3000@gmail.com>
2022-09-09 10:02:07 -0700

    small chnges

--- layouts/plain.vue
+++ layouts/plain.vue
@@ -3,7 +3,7 @@
       class="
         flex flex-col
         min-h-screen scroll-smooth
-        font-montserrat
+        font-[Poppins]
         text-lg
         tracking-wide
         text-gray-900
@@ -13,8 +13,8 @@
       <main class="flex-1 w-full mx-auto bg-white"><slot /></main>
     </div>
   </template>
-      <style>
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
   </style>
   
   <script setup>
