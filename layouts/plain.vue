480fb27ae5ca440f2e2d98f96d4c3717290413f9
raghuram <airaghuram3000@gmail.com>
2022-09-09 10:15:43 -0700

    changes

--- layouts/plain.vue
+++ layouts/plain.vue
@@ -13,6 +13,7 @@
       <main class="flex-1 w-full mx-auto bg-white"><slot /></main>
     </div>
   </template>
+  
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
   </style>
